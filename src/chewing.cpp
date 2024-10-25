d983eb6aaaff1ca390bf0737ecef13133d77da24
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-10-25 12:32:39 +0900

    Add chewing

---
+++ src/chewing.cpp
@@ -0,0 +1,34 @@
+#include <M5Unified.h>
+
+#define CYCLES 15
+
+void setup(void) {
+  auto cfg = M5.config();
+  M5.begin(cfg);
+  M5.delay(500);  // Serial出力用のウエイト
+
+  // 画面上にログを表示する
+  M5.setLogDisplayIndex(0);
+  M5.Display.setTextScroll(true);
+}
+
+void loop(void) {
+  M5.delay(1);
+  M5.update();
+  if (M5.BtnB.wasPressed()) {
+    // 右側のボタンを押して離した    
+    digitalWrite(ODOR_PIN, LOW);
+    tone(GPIO_NUM_2, 600, 1000);  // 600Hzを1000ms(1秒)
+    M5.Log.println("BtnA TONE(600Hz)");   
+  }
+  if(M5.BtnA.wasPressed()){
+    // 真ん中のボタンを押して離した
+    M5.Log.println("BtnB");
+    for (int i = 0; i < CYCLES; i++) {
+      digitalWrite(ODOR_PIN, LOW);
+      tone(GPIO_NUM_2, 600, 1000);  // 600Hzを1000ms(1秒)      
+      delay(10000);
+    }
+    M5.Log.println("Done");
+  }
+}
